43b921c26e4534099d31c0c855544ef91d0a884b
nihalanas19 <nihalanas19@example.com>
2026-07-12 18:37:12 +0530

    fix: correct class name and improve spell casting output

--- Pyhton.py
+++ Pyhton.py
@@ -1,17 +1,20 @@
-import random
 import time
-class wizard:
+import random
+class Wizard:
     def __init__(self, name):
         self.name = name
-        
-    def cast_spell(self):
-        print(self.name + "  is casting a spell...!")
-        time.sleep(2)
-        
-        damage = random.randint(0, 50)
 
+    def cast_spell(self):
+        print(self.name + " casts a spell!")
+        time.sleep(1)
+
+        damage = random.randint(0,50)
         print("🔥Fireball")
         print("Damage:", damage)
 
-wizard1 = wizard("peter")
+wizard1=Wizard("Nihal")
+wizard2=Wizard("Merlin")
+wizard3=Wizard("Harry")
 wizard1.cast_spell()
+wizard2.cast_spell()
+wizard3.cast_spell()
