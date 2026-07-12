8b4b550ddf4003dfbea686a9c24c930bc022a5e6
nihalanas19 <nihalanas19@example.com>
2026-07-12 16:09:18 +0530

    Initial commit

---
+++ Pyhton.py
@@ -0,0 +1,17 @@
+import random
+import time
+class wizard:
+    def __init__(self, name):
+        self.name = name
+        
+    def cast_spell(self):
+        print(self.name + "  is casting a spell...!")
+        time.sleep(2)
+
+        damage = random.randint(0, 50)
+
+        print("🔥Fireball")
+        print("Damage:", damage)
+
+wizard1 = wizard("peter")
+wizard1.cast_spell()
